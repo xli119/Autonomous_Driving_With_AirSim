60f6a86c664449f97c6bea3fc2075d57f4af3af8
LYuan <yuluo1203@gmail.com>
2018-06-20 22:38:15 +0800

    Update Image_processing.py

--- Image_processing.py
+++ Image_processing.py
@@ -70,3 +70,35 @@ def get_image():
     
     return image_rgba[76:135,0:255,0:3].astype(float)
 '''
+'''
+def get_image():
+    image_response = client.simGetImages([ImageRequest(0, AirSimImageType.Scene, False, False)])[0]
+    image1d = np.fromstring(image_response.image_data_uint8, dtype=np.uint8)
+    image_rgba = image1d.reshape(image_response.height, image_response.width, 4)
+
+    
+    return image_rgba
+    
+  while (True):
+    car_state = client.getCarState()
+    
+    if (car_state.speed < 5):
+        car_controls.throttle = 1.0
+    else:
+        car_controls.throttle = 0.0
+    
+    image_temp = get_image()
+    img = Image.fromarray(image_temp,'RGBA')
+    contrast = 2.0
+    image_rgba = ImageEnhance.Contrast(img).enhance(contrast)
+    image_buffer = np.array(image_rgba, dtype = np.float64)
+    
+    image_buf[0] = image_buffer[76:135,0:255,0:3].astype(float)
+    state_buf[0] = np.array([car_controls.steering, car_controls.throttle, car_controls.brake, car_state.speed])
+    model_output = model.predict([image_buf, state_buf])
+    car_controls.steering = round(0.5 * float(model_output[0][0]), 2)
+    
+    print('Sending steering = {0}, throttle = {1}'.format(car_controls.steering, car_controls.throttle))
+    
+    client.setCarControls(car_controls)
+   '''
