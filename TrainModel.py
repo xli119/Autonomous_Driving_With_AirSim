7ebf509785f80b898012bb4f3b934533c968f085
Xiaoyu Li <35972960+xli119@users.noreply.github.com>
2018-07-16 14:43:18 -0400

    Add files via upload
    training models with different parameters

--- TrainModel.py
+++ TrainModel.py
@@ -29,123 +29,115 @@ config.gpu_options.allow_growth = True
 session = tf.Session(config=config)
 
 # << The directory containing the cooked data from the previous step >>
-COOKED_DATA_DIR = 'D:\EndToEndLearningRawData\data_cooked'
-
-# << The directory in which the model output will be placed >>
-MODEL_OUTPUT_DIR = 'D:/EndToEndLearningRawData/model_output/drop=0.5_bright=0.4_shadow_300.30.9'
+COOKED_DATA_DIR = 'D:\EndToEndLearningRawData\data_cooked_nh'
 
 
-train_dataset = h5py.File(os.path.join(COOKED_DATA_DIR, 'train.h5'), 'r')
-eval_dataset = h5py.File(os.path.join(COOKED_DATA_DIR, 'eval.h5'), 'r')
-test_dataset = h5py.File(os.path.join(COOKED_DATA_DIR, 'test.h5'), 'r')
-
-num_train_examples = train_dataset['image'].shape[0]
-num_eval_examples = eval_dataset['image'].shape[0]
-num_test_examples = test_dataset['image'].shape[0]
-
-batch_size=32
-
-# We choose some images randomly and make them darker
-random_dark = np.random.uniform(low=0.3, high=0.9)
-for i in range(num_train_examples):
-    p=0.3
-    r = random.random()
-    if r<p:
-        train_dataset['image'][i] = cv2.cvtColor(train_dataset['image'][i], cv2.COLOR_RGB2HSV)
-        train_dataset['image'][i][:, :, 2] = np.clip(train_dataset['image'][i][:, :, 2] * random_dark, 0, 255)
-        train_dataset['image'][i] = cv2.cvtColor(train_dataset['image'][i], cv2.COLOR_HSV2RGB)
+for bright in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
+    # << The directory in which the model output will be placed >>
+    MODEL_OUTPUT_DIR = 'D:/EndToEndLearningRawData/model_final_nh/nROI60140Drop=0.0_bright=' + str(bright)
 
 
-data_generator = DriveDataGenerator(rescale=1./255., horizontal_flip=True, brighten_range=0.4)
-train_generator = data_generator.flow\
-    (train_dataset['image'], train_dataset['previous_state'], train_dataset['label'], batch_size=batch_size, zero_drop_percentage=0.5, roi=[76,135,0,255])
-eval_generator = data_generator.flow\
-    (eval_dataset['image'], eval_dataset['previous_state'], eval_dataset['label'], batch_size=batch_size, zero_drop_percentage=0.5, roi=[76,135,0,255])
+    train_dataset = h5py.File(os.path.join(COOKED_DATA_DIR, 'train.h5'), 'r')
+    eval_dataset = h5py.File(os.path.join(COOKED_DATA_DIR, 'eval.h5'), 'r')
+    test_dataset = h5py.File(os.path.join(COOKED_DATA_DIR, 'test.h5'), 'r')
+
+    num_train_examples = train_dataset['image'].shape[0]
+    num_eval_examples = eval_dataset['image'].shape[0]
+    num_test_examples = test_dataset['image'].shape[0]
+
+    batch_size=32
 
 
-def draw_image_with_label(img, label, prediction=None):
-    theta = label * 0.69  # Steering range for the car is +- 40 degrees -> 0.69 radians
-    line_length = 50
-    line_thickness = 3
-    label_line_color = (255, 0, 0)
-    prediction_line_color = (0, 0, 255)
-    pil_image = image.array_to_img(img, K.image_data_format(), scale=True)
-    print('Actual Steering Angle = {0}'.format(label))
-    draw_image = pil_image.copy()
-    image_draw = ImageDraw.Draw(draw_image)
-    first_point = (int(img.shape[1] / 2), img.shape[0])
-    second_point = (
-    int((img.shape[1] / 2) + (line_length * math.sin(theta))), int(img.shape[0] - (line_length * math.cos(theta))))
-    image_draw.line([first_point, second_point], fill=label_line_color, width=line_thickness)
+    data_generator = DriveDataGenerator(rescale=1./255., horizontal_flip=True, brighten_range=bright)
+    train_generator = data_generator.flow\
+        (train_dataset['image'], train_dataset['previous_state'], train_dataset['label'], batch_size=batch_size, zero_drop_percentage=0.0, roi=[60,140,0,255])
+    eval_generator = data_generator.flow\
+        (eval_dataset['image'], eval_dataset['previous_state'], eval_dataset['label'], batch_size=batch_size, zero_drop_percentage=0.0, roi=[60,140,0,255])
 
-    if (prediction is not None):
-        print('Predicted Steering Angle = {0}'.format(prediction))
-        print('L1 Error: {0}'.format(abs(prediction - label)))
-        theta = prediction * 0.69
+
+    def draw_image_with_label(img, label, prediction=None):
+        theta = label * 0.69  # Steering range for the car is +- 40 degrees -> 0.69 radians
+        line_length = 50
+        line_thickness = 3
+        label_line_color = (255, 0, 0)
+        prediction_line_color = (0, 0, 255)
+        pil_image = image.array_to_img(img, K.image_data_format(), scale=True)
+        print('Actual Steering Angle = {0}'.format(label))
+        draw_image = pil_image.copy()
+        image_draw = ImageDraw.Draw(draw_image)
+        first_point = (int(img.shape[1] / 2), img.shape[0])
         second_point = (
         int((img.shape[1] / 2) + (line_length * math.sin(theta))), int(img.shape[0] - (line_length * math.cos(theta))))
-        image_draw.line([first_point, second_point], fill=prediction_line_color, width=line_thickness)
+        image_draw.line([first_point, second_point], fill=label_line_color, width=line_thickness)
 
-    del image_draw
-    plt.imshow(draw_image)
-    plt.show()
+        if (prediction is not None):
+            print('Predicted Steering Angle = {0}'.format(prediction))
+            print('L1 Error: {0}'.format(abs(prediction - label)))
+            theta = prediction * 0.69
+            second_point = (
+            int((img.shape[1] / 2) + (line_length * math.sin(theta))), int(img.shape[0] - (line_length * math.cos(theta))))
+            image_draw.line([first_point, second_point], fill=prediction_line_color, width=line_thickness)
 
-
-[sample_batch_train_data, sample_batch_test_data] = next(train_generator)
-for i in range(0, 3, 1):
-    draw_image_with_label(sample_batch_train_data[0][i], sample_batch_test_data[i])
-
-
-image_input_shape = sample_batch_train_data[0].shape[1:]
-state_input_shape = sample_batch_train_data[1].shape[1:]
-activation = 'relu'
-
-#Create the convolutional stacks
-pic_input = Input(shape=image_input_shape)
-
-img_stack = Conv2D(16, (3, 3), name="convolution0", padding='same', activation=activation)(pic_input)
-img_stack = MaxPooling2D(pool_size=(2,2))(img_stack)
-img_stack = Conv2D(32, (3, 3), activation=activation, padding='same', name='convolution1')(img_stack)
-img_stack = MaxPooling2D(pool_size=(2, 2))(img_stack)
-img_stack = Conv2D(32, (3, 3), activation=activation, padding='same', name='convolution2')(img_stack)
-img_stack = MaxPooling2D(pool_size=(2, 2))(img_stack)
-img_stack = Flatten()(img_stack)
-img_stack = Dropout(0.2)(img_stack)
-
-#Inject the state input
-state_input = Input(shape=state_input_shape)
-merged = concatenate([img_stack, state_input])
-
-# Add a few dense layers to finish the model
-merged = Dense(64, activation=activation, name='dense0')(merged)
-merged = Dropout(0.2)(merged)
-merged = Dense(10, activation=activation, name='dense2')(merged)
-merged = Dropout(0.2)(merged)
-merged = Dense(1, name='output')(merged)
-
-adam = Nadam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
-model = Model(inputs=[pic_input, state_input], outputs=merged)
-model.compile(optimizer=adam, loss='mse')
-
-model.summary()
+        del image_draw
+        plt.imshow(draw_image)
+        plt.show()
 
 
-plateau_callback = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=3, min_lr=0.0001, verbose=1)
-checkpoint_filepath = os.path.join(MODEL_OUTPUT_DIR, 'models', '{0}_model.{1}-{2}.h5'.format('model', '{epoch:02d}', '{val_loss:.7f}'))
-checkAndCreateDir(checkpoint_filepath)
-checkpoint_callback = ModelCheckpoint(checkpoint_filepath, save_best_only=True, verbose=1)
-csv_callback = CSVLogger(os.path.join(MODEL_OUTPUT_DIR, 'training_log.csv'))
-early_stopping_callback = EarlyStopping(monitor='val_loss', patience=10, verbose=1)
-callbacks=[plateau_callback, csv_callback, checkpoint_callback, early_stopping_callback, TQDMNotebookCallback()]
+    [sample_batch_train_data, sample_batch_test_data] = next(train_generator)
+    for i in range(0, 3, 1):
+        draw_image_with_label(sample_batch_train_data[0][i], sample_batch_test_data[i])
 
 
-history = model.fit_generator(train_generator, steps_per_epoch=num_train_examples//batch_size, epochs=500, callbacks=callbacks,\
-                   validation_data=eval_generator, validation_steps=num_eval_examples//batch_size, verbose=2)
+    image_input_shape = sample_batch_train_data[0].shape[1:]
+    state_input_shape = sample_batch_train_data[1].shape[1:]
+    activation = 'relu'
+
+    #Create the convolutional stacks
+    pic_input = Input(shape=image_input_shape)
+
+    img_stack = Conv2D(16, (3, 3), name="convolution0", padding='same', activation=activation)(pic_input)
+    img_stack = MaxPooling2D(pool_size=(2,2))(img_stack)
+    img_stack = Conv2D(32, (3, 3), activation=activation, padding='same', name='convolution1')(img_stack)
+    img_stack = MaxPooling2D(pool_size=(2, 2))(img_stack)
+    img_stack = Conv2D(32, (3, 3), activation=activation, padding='same', name='convolution2')(img_stack)
+    img_stack = MaxPooling2D(pool_size=(2, 2))(img_stack)
+    img_stack = Flatten()(img_stack)
+    img_stack = Dropout(0.2)(img_stack)
+
+    #Inject the state input
+    state_input = Input(shape=state_input_shape)
+    merged = concatenate([img_stack, state_input])
+
+    # Add a few dense layers to finish the model
+    merged = Dense(64, activation=activation, name='dense0')(merged)
+    merged = Dropout(0.2)(merged)
+    merged = Dense(10, activation=activation, name='dense2')(merged)
+    merged = Dropout(0.2)(merged)
+    merged = Dense(1, name='output')(merged)
+
+    adam = Nadam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+    model = Model(inputs=[pic_input, state_input], outputs=merged)
+    model.compile(optimizer=adam, loss='mse')
+
+    model.summary()
 
 
-[sample_batch_train_data, sample_batch_test_data] = next(train_generator)
-predictions = model.predict([sample_batch_train_data[0], sample_batch_train_data[1]])
-for i in range(0, 3, 1):
-    draw_image_with_label(sample_batch_train_data[0][i], sample_batch_test_data[i], predictions[i])
+    plateau_callback = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=3, min_lr=0.0001, verbose=1)
+    checkpoint_filepath = os.path.join(MODEL_OUTPUT_DIR, 'models', '{0}_model.{1}-{2}.h5'.format('model', '{epoch:02d}', '{val_loss:.7f}'))
+    checkAndCreateDir(checkpoint_filepath)
+    checkpoint_callback = ModelCheckpoint(checkpoint_filepath, save_best_only=False, verbose=1)
+    csv_callback = CSVLogger(os.path.join(MODEL_OUTPUT_DIR, 'training_log.csv'))
+    early_stopping_callback = EarlyStopping(monitor='val_loss', patience=10, verbose=1)
+    callbacks=[plateau_callback, csv_callback, checkpoint_callback, early_stopping_callback, TQDMNotebookCallback()]
+
+
+    history = model.fit_generator(train_generator, steps_per_epoch=num_train_examples//batch_size, epochs=500, callbacks=callbacks,\
+                       validation_data=eval_generator, validation_steps=num_eval_examples//batch_size, verbose=2)
+
+
+    [sample_batch_train_data, sample_batch_test_data] = next(train_generator)
+    predictions = model.predict([sample_batch_train_data[0], sample_batch_train_data[1]])
+    for i in range(0, 3, 1):
+        draw_image_with_label(sample_batch_train_data[0][i], sample_batch_test_data[i], predictions[i])
 
 
